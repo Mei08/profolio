a2e2a5403df6f75123996d9244fe20c7eb46c613
Mei08 <125234mary@gmail.com>
2016-02-22 15:28:56 -0500

    1

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'meiHe' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+module.exports = router;
